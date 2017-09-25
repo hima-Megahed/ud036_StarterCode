96a7ec8c81334781a5f6a0ab2a7ef2747b5afee3
ibrahim hasan <elmasrye67@hotmail.com>
2017-09-25 21:47:03 +0200

    Correcting indentaions, adding comments and updating READ ME

--- Backend.py
+++ Backend.py
@@ -4,9 +4,12 @@ import os
 class Video:
     """Parent class used to hold information about any generic video"""
 
-    RATINGS = ["G", "PG", "PG-13", "R"]
+    RATINGS = ["G", "PG", "PG-13", "R"]   # valid ratings
 
     def __init__(self, title, duration, genre, year, description):
+        """Constructor method for video class initialize a new instance of
+        this class Parameters: video title, duration, genre, year,
+        description """
         self.Title = title
         self.Duration = duration
         self.Genre = genre
@@ -14,6 +17,8 @@ class Video:
         self.Description = description
 
     def show_video_info(self):
+        """this function is responsible about displaying video information like :
+        video title, duration, genre, release year, story line"""
         print("Video Title is: " + self.Title)
         print("Video Duration is: " + self.Duration)
         print("Video Genre is: " + self.Genre)
@@ -22,34 +27,54 @@ class Video:
 
 
 class Movie(Video):
-    """Movie Class Used to express a movie object, holds attributes of movies"""
-    def __init__(self, movie_title, movie_duration, movie_genre, movie_year, movie_description,
+    """Movie Class Used to express a movie object,
+     holds attributes of movies"""
+
+    def __init__(self, movie_title, movie_duration, movie_genre, movie_year,
+                 movie_description,
                  movie_poster_image, movie_youtube_trailer):
-        Video.__init__(self, movie_title, movie_duration, movie_genre, movie_year, movie_description)
+        """Constructor method for Movie class initialize a new instance of
+        this class, Parameters: Movie title, duration, genre, year,
+        description, poster image, youtube trailer """
+        Video.__init__(self, movie_title, movie_duration, movie_genre,
+                       movie_year, movie_description)
         self.PosterImage = movie_poster_image
         self.YoutubeTrailer = movie_youtube_trailer
 
     def show_trailer(self):
+        """this function is responsible about showing the movie trailer"""
         os.system("start chrome " + self.YoutubeTrailer)
 
 
 class Series(Video):
-    """Class Series Used to express series ,holds information about the series. """
-    def __init__(self,  series_title, series_duration, series_genre, series_year, series_description,
-                 series_episode_number, series_season, series_poster_image, series_youtube_trailer):
-        Video.__init__(self, series_title, series_duration, series_genre, series_year, series_description)
+    """Class Series Used to express series,
+    holds information about the series. """
+    def __init__(self,  series_title, series_duration, series_genre,
+                 series_year, series_description,
+                 series_episode_number, series_season,
+                 series_poster_image, series_youtube_trailer):
+        """Constructor method for Series class initialize a new instance of
+                this class, Parameters: series title, duration, genre, year,
+                description, episode number, season number,
+                poster image, youtube trailer"""
+        Video.__init__(self, series_title, series_duration,
+                       series_genre, series_year, series_description)
         self.EpisodeNumber = series_episode_number
         self.Season = series_season
         self.PosterImage = series_poster_image
         self.YoutubeTrailer = series_youtube_trailer
 
     def show_trailer(self):
+        """this function is responsible about showing the Series trailer"""
         os.system("start chrome " + self.YoutubeTrailer)
 
     def show_video_info(self):
-        print("Video Title is: " + self.Title)
-        print("Video Duration is: " + self.Duration)
-        print("Video Genre is: " + self.Genre)
+        """this function is responsible about showing the Series information
+        such that series title, duration, genre, release year, story line,
+        episode number and season number"""
+        print("series Title is: " + self.Title)
+        print("series Duration is: " + self.Duration)
+        print("series Genre is: " + self.Genre)
         print("Year of release: " + self.Year)
         print("Description: " + self.Description)
         print ("Episode Number: " + self.EpisodeNumber)
--- README.md
+++ README.md
@@ -3,12 +3,19 @@
 Movie Trailer Website is a website constructed using python,
 allowing users to surf the latest movies and watch the trailer.
 
-<h5>Installation</h5>
-download the file and just open Movie Trailer.html,
-if you want to add some movies you must first install python 2.7 or higher
-then you open main.py and add a new instance from Movie class or Series class.
+<h5>Installation!!</h5>
+<ul>
+<li>Install python 2.7 or higher.</li>
+<li>Download the project as a zip file. </li>
+</ul>
 
-
+<h5>How to generate the page?</h5>
+<ul>
+<li>Open IDLE (python GUI) then Hit <strong>CTRL + O</strong>.</li>
+<li>Choose the location of the downloaded file then choose <strong>main.py </strong>then click open.</li>
+<li>Hit <b>F5</b></li>
+</ul>
+<br> <br> 
 <h5>Contacts</h5>
 Email: Ibrahim__Hasan@outlook.com
 
